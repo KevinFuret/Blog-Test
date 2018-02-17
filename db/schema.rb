996536253a2cb9bbc55eeade5d8ed8029e4b612a
kefuret <kefuret@gmail.com>
2018-02-17 18:28:48 +0100

    :sparkles: Add controllers and a little bit of style

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180217163600) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users", name: "user_on_post"
