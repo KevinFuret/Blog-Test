e8e1929cb30485dd054bf9807ed9f733857ef725
kefuret <kefuret@gmail.com>
2018-02-17 20:20:49 +0100

    :art: Some style on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :user
   before_action :configure_devise_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def user
     @user = current_user
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "light"
+    else
+      "application"
+    end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,49 @@
 class PostsController < ApplicationController
 
+  before_action  only: [:show, :update, :update, :destroy]
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all
+    if Post.last.nil? == false
+      @latest_post = Post.last
+      @posts = Post.where.not(id: @latest_post.id).order('created_at DESC')
+    else
+      @posts = Post.all
+    end
   end
 
   def new
     @post = current_user.posts.build
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to "/"
+    end
+  end
+
+  def update
+    Post.find(params[:id]).update(post_params)
+    redirect_to "/post/#{params[:id]}"
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
+  end
 end
