4e80ece519a1dc27dee88c71315b2342667b3211
kefuret <kefuret@gmail.com>
2018-02-17 18:48:04 +0100

    :art: Url by username instead of id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:username])
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:username])
     @posts = current_user.posts.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
+  get 'users/:username' => 'users#show'
 end
